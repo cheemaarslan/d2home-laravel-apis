ce60b242eade5f4b082f654f90b5e5eca4ce4dd9
Huzaifa Gulzar <huzaifa6715@gmail.com>
2025-06-25 13:26:38 +0500

    Changes added

---
+++ database/migrations/2025_06_23_140653_create_delivery_man_weekly_reports_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDeliveryManWeeklyReportsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+       Schema::create('delivery_man_weekly_reports', function (Blueprint $table) {
+        $table->id();
+        $table->unsignedBigInteger('delivery_man_id');
+        $table->string('week_identifier');
+        $table->json('order_ids');
+        $table->decimal('total_price', 10, 2);
+        $table->integer('orders_count');
+        $table->decimal('total_commission', 10, 2);
+        $table->decimal('total_discounts', 10, 2);
+        $table->timestamps();
+
+        $table->foreign('delivery_man_id')->references('id')->on('users')->onDelete('cascade');
+    });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('delivery_man_weekly_reports');
+    }
+}
